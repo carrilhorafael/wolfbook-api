e5574da5c8ed8929740f034faed5b7be3ccb1050
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-15 10:11:25 -0300

    validate speakers must be friends

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,12 @@ class Conversation < ApplicationRecord
   belongs_to :speaker1, class_name: "User"
   belongs_to :speaker2, class_name: "User"
   has_many :messages
+  validate :speakers_must_be_friends
+
+  def speakers_must_be_friends
+    friendship = Friendship.find_by(user_id: speaker1.id, friend_id: speaker2.id, accept: true)
+    if friendship.nil?
+      errors.add(:friends, "This speakers arent friends")
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name:"Rafael Carrilho", role: 1, email:"rafael@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
-User.create(name:"Arthur Nakao", role: 1, email:"arthur@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
-User.create(name:"Rafael Kanazawa", role: 2, email:"kana@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
-User.create(name:"Mairon Azevedo", role: 2, email:"mairon@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
-User.create(name:"Thiago Andrade", role: 2, email:"thiago@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
+User.create(name:"Rafael Carrilho", role: 3, email:"rafael@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
+User.create(name:"Arthur Nakao", role: 3, email:"arthur@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
+User.create(name:"Rafael Kanazawa", role: 3, email:"kana@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
+User.create(name:"Mairon Azevedo", role: 3, email:"mairon@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
+User.create(name:"Thiago Andrade", role: 3, email:"thiago@email.com", gender:"M", birthdate:"16/02/2000", password:"123456", password_confirmation:"123456")
 
 require 'faker'
 20.times.each do |t|
