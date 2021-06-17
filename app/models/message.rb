03e988569b520b764e1739b6df79d7552fb09297
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-17 13:16:18 -0300

    change transmit to conversation messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :sender, class_name: "User", foreign_key: "sender_id"
-  after_create {ConversationChannel.broadcast_to("ChatRoom-#{conversation.id}", MessageSerializer.new(self))}
+  after_create {ConversationChannel.broadcast_to("ChatRoom-#{conversation.id}", conversation.messages)}
   validate :sender_on_conversation
   def sender_on_conversation
     if sender.id != conversation.speaker1.id && sender.id != conversation.speaker2.id
